2a25492dc6dd86e51710c7cb8e8f9bfc80c44ace
Glozano26 <lozsuperbia2014@gmail.com>
2023-12-29 12:36:58 -0500

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,2 +1,21 @@
+#!/usr/bin/python3
+"""Contains entrypoint"""
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def clean_up_all(exc):
+    """Cleaning function to be executed at
+    end application context"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
+
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+app_views = Flask(__name__)
+
+@app_views.route('/status')
+def status():
+    """"functions that return JSON"""
+    data = {'status': 'OK'}
+    return jsonify(data)
