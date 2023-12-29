606352c496f5448a2c74211e172c6dd8bde48504
Glozano26 <lozsuperbia2014@gmail.com>
2023-12-29 14:16:59 -0500

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Contains the app_views
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 # app_views = Flask(__name__)
 
@@ -13,3 +14,8 @@ def status():
     """"functions that return JSON"""
     data = {'status': 'OK'}
     return jsonify(data)
+
+@app_views.route('/api/v1/stats')
+def counts():
+    """Create an endpoint that retrieves the number of each objects by type"""
+    storage.count()
