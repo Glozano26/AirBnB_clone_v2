03d24a895ae2314191110ae21117fd4313a29ce5
Glozano26 <lozsuperbia2014@gmail.com>
2023-12-29 15:20:50 -0500

    Some stats v7 update route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -26,7 +26,7 @@ def counts():
     num_objs = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
-        "places": storage.count("Places"),
+        "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
