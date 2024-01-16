b94395827ad0871e4721f29f7705f4ca6bfab61f
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-15 20:11:49 -0500

    updated method PUT

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -62,15 +62,20 @@ def create_new_city(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
-def update_city_data(city_id):
-    '''Update the values of the attributes of a city'''
+def update_city(city_id):
+    """Updates a City object"""
     city = storage.get(City, city_id)
     if city is None:
         return {'error': 'Not found'}, 404
-    if request.is_json:
-        data = request.get_json()
-        for key, value in data.items():
+
+    cities_HTTP = request.get_json()
+
+    if cities_HTTP is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+
+    keys_ignored = ['id', 'state_id', 'created_at', 'updated_at']
+    for key, value in cities_HTTP.items():
+        if key not in keys_ignored:
             setattr(city, key, value)
-        storage.save()
-        return jsonify(city.to_dict()), 200
-    return {'error': 'Not a JSON'}, 400
+    storage.save()
+    return jsonify(city.to_dict()), 200
